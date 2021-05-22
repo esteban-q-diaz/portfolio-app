9fa9a4ebb8477b224fedcdfa4fd4d2db0d47b095
esteban-q-diaz <estebandpal@gmail.com>
2021-05-22 00:48:59 -0700

    changed style

---
+++ Documents/Hack_Reactor/portfolio/apps/web/portfolio-app/client/components/Projects.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <>
+      <div className="projects">
+        <div className="project-cards">
+          <div className="project-image">
+            <img src='https://general-ez.s3-us-west-1.amazonaws.com/spoti.png' />
+          </div>
+          <div className="project-text">
+            <h1>Spotioke</h1>
+            <p>An app that does... and .... and ...</p>
+        </div>
+
+        </div>
+
+
+      </div>
+    </>
+  )
+}
+
+export default Projects
